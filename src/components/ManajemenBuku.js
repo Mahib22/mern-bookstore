2359528833e26a31e8ec724d6130f38acb04c2ef
Mahib22 <mahib.arib@gmail.com>
2022-10-30 10:28:12 +0700

    fetching api

--- src/components/ManajemenBuku.js
+++ src/components/ManajemenBuku.js
@@ -1,10 +1,83 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TabelBuku } from "./TabelBuku";
+import axios from "axios";
 
 export const ManajemenBuku = () => {
   const [formMode, setFormMode] = useState("");
+  const [books, setBooks] = useState([]);
+  const [inputForm, setInputForm] = useState();
+
   const showCreateForm = () => {
-    setFormMode("show");
+    setInputForm("");
+    setFormMode("create");
+  };
+
+  const showEditForm = (book) => {
+    setInputForm(book);
+    setFormMode("edit");
+  };
+
+  const handleTitle = (e) => {
+    setInputForm({ ...inputForm, title: e.target.value });
+  };
+
+  const handleAuthor = (e) => {
+    setInputForm({ ...inputForm, author: e.target.value });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    if (formMode === "create") {
+      axios
+        .post("http://localhost:4000/book/add", inputForm)
+        .then(() => {
+          alert("Data berhasil ditambahkan");
+          retreiveData();
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    }
+
+    if (formMode === "edit") {
+      axios
+        .put(`http://localhost:4000/book/update/${inputForm._id}`, inputForm)
+        .then(() => {
+          retreiveData();
+          alert("Data berhasil diubah");
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    }
+  };
+
+  const deleteOne = (book) => {
+    axios
+      .delete(`http://localhost:4000/book/delete/${book._id}`)
+      .then(() => {
+        retreiveData();
+        alert("Data berhasil dihapus");
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  useEffect(() => {
+    retreiveData();
+  }, []);
+
+  const retreiveData = () => {
+    axios
+      .get("http://localhost:4000/book")
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
   };
 
   return (
@@ -15,17 +88,19 @@ export const ManajemenBuku = () => {
         Tambah Buku
       </button>
 
-      {formMode === "show" ? (
+      {formMode !== "" ? (
         <div id="form" className="card py-2 my-3 bg-secondary">
           <div className="card-body">
             <h4>Form Buku</h4>
-            <form className="row">
+            <form className="row" onSubmit={submitForm}>
               <div className="col-md-6">
                 <input
                   type="text"
                   className="form-control mx-2"
                   name="title"
                   placeholder="Judul..."
+                  onChange={handleTitle}
+                  value={inputForm.title || ""}
                 />
               </div>
               <div className="col-md-4">
@@ -34,6 +109,8 @@ export const ManajemenBuku = () => {
                   className="form-control mx-2"
                   name="author"
                   placeholder="Penulis..."
+                  onChange={handleAuthor}
+                  value={inputForm.author || ""}
                 />
               </div>
               <div className="col-md-2">
@@ -46,7 +123,11 @@ export const ManajemenBuku = () => {
         </div>
       ) : null}
 
-      <TabelBuku showEdit={showCreateForm} />
+      <TabelBuku
+        showEdit={showEditForm}
+        books={books}
+        requestToDelete={deleteOne}
+      />
     </div>
   );
 };
--- src/components/TabelBuku.js
+++ src/components/TabelBuku.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-export const TabelBuku = ({ showEdit }) => {
+export const TabelBuku = ({ showEdit, books, requestToDelete }) => {
+  const deleteData = (book) => {
+    requestToDelete(book);
+  };
+
   return (
     <div>
       <h4>Tabel Data Buku</h4>
@@ -14,20 +18,27 @@ export const TabelBuku = ({ showEdit }) => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Laskar Pelangi</td>
-            <td>Andrea Hirata</td>
-            <td>
-              <button
-                className="btn btn-sm btn-warning mx-2"
-                onClick={() => showEdit()}
-              >
-                Edit
-              </button>
-              <button className="btn btn-sm btn-danger">Delete</button>
-            </td>
-          </tr>
+          {books.map((book, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>
+                <button
+                  className="btn btn-sm btn-warning mx-2"
+                  onClick={() => showEdit(book)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="btn btn-sm btn-danger"
+                  onClick={() => deleteData(book)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
